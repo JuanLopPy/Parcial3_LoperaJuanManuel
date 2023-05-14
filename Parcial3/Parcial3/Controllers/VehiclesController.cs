ee70436dbb76d781c37eb729dd2661b732e14be9
juanm <juanm@JUANMA>
2023-05-14 10:29:47 -0500

    code improvement

--- Parcial3/Parcial3/Controllers/VehiclesController.cs
+++ Parcial3/Parcial3/Controllers/VehiclesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -44,7 +45,7 @@ namespace Parcial3.Controllers
 
             return View(vehicle);
         }
-
+        
         // GET: Vehicles/Create
         public IActionResult Create()
         {
